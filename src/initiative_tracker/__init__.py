73c6eadc96144559066f2809d099a8d77417d83b
Hercules Konsoulas <dyka3773@gmail.com>
2026-03-18 15:15:35 +0100

    feat: scaffold project files

---
+++ src/initiative_tracker/__init__.py
@@ -0,0 +1,5 @@
+from initiative_tracker import server
+
+__all__ = [
+    "server",
+]
---
+++ src/initiative_tracker/server.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 5-5e-initiative-tracker!")
+
+
+if __name__ == "__main__":
+    main()
